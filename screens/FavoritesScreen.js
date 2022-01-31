a88257c47738e2763e5abae9e68bcb890ffc378b
Trung Kien Huynh <trungkien.huynh@gmail.com>
2022-01-31 10:53:15 +0100

    test commit

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from "react-native";
 export default function FavoritesScreen(props) {
   return (
     <View>
-      <Text>Favorites</Text>
+      <Text>Favorites yahoo</Text>
     </View>
   );
 }
